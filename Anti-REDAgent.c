860dcf6afbcc6014d9101199ec6aba991d5a532c
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-01 18:18:07 +0800

    Compatible C language

--- Anti-REDAgent.c
+++ Anti-REDAgent.c
@@ -1,11 +1,9 @@
 #define _WIN32_WINNT 0x0500
-#include <iostream>
 #include <windows.h>
-using namespace std;
 int main() {
 	HWND hWnd = GetConsoleWindow();
 	ShowWindow( hWnd, SW_HIDE );
-	while(true) {
+	while(1) {
 		system("taskkill /f /im REDAgent.exe");
 		system("taskkill /f /im ePointer.exe");
 		system("taskkill /f /im ngtray.exe");
